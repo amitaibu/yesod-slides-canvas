d4060fb82ac8933ce483470dc5e68424135d9dfc
Amitai Burstein <amitai@gizra.com>
2016-10-26 21:40:12 +0300

    Add Api.Slide

---
+++ Handler/Api/Slide.hs
@@ -0,0 +1,6 @@
+module Handler.Api.Slide where
+
+import           Import
+
+getSlideR :: SlideId -> Handler Html
+getSlideR slideId = error "Not yet implemented: getSlideR"
---
+++ test/Handler/SlideSpec.hs
@@ -0,0 +1,10 @@
+module Handler.SlideSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getSlideR" $ do
+        error "Spec not implemented: getSlideR"
+
