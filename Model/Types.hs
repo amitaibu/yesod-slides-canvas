458b7a184e0082efe9709ea7dbf318367ddee4a0
Amitaibu <amitai@gizra.com>
2016-10-26 13:42:15 +0300

    Fix types

--- Model/Types.hs
+++ Model/Types.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Model.Types where
 
 import Prelude
 import Yesod
+import GHC.Generics
 
 data MediaType = MediaImage | MediaYouTube
-    deriving (Show, Read, Eq, Enum, Bounded)
+    deriving (Show, Read, Eq, Enum, Bounded, Generic)
 
-derivePersistField "MediaType"    
+derivePersistField "MediaType"
+
+instance ToJSON MediaType
+instance FromJSON MediaType
