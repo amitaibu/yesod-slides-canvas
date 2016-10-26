f8b157d06d5feef676ca5ae5c81a3d2c853e258c
Amitai Burstein <amitai@gizra.com>
2016-10-26 23:11:27 +0300

    Intermidate

--- Handler/Api/Slide.hs
+++ Handler/Api/Slide.hs
@@ -1,9 +1,33 @@
 module Handler.Api.Slide where
 
+import qualified Data.HashMap.Strict as HM
 import           Import
+
+
+-- addEmbeds :: SlideId
+--           -> Slide
+--           -> Value
+--           -> Maybe (HashMap Text Value)
+addEmbeds slideId slide embeds =
+    case toJSON (Entity slideId slide) of
+        Object obj -> Just $ HM.insert "embeds" (object [("foo", String "Bar")]) obj
+        -- Object obj -> Just obj
+        _          -> Nothing
+
+valueToHash :: (ToJSON (Entity record), PersistEntity record)
+            => Key record
+            -> record -> Maybe (HashMap Text Value)
+valueToHash key record =
+    case toJSON (Entity key record) of
+        Object obj -> Just obj
+        _          -> Nothing
+
 
 getSlideR :: SlideId -> Handler Value
 getSlideR slideId = do
     slide <- runDB $ get404 slideId
 
-    return $ object ["data" .= slide]
+    embedsBySlide <- runDB $ selectList [SlideId ==. slideId] []
+    let slideWitheEmbeds = addEmbeds slideId slide embedsBySlide
+
+    return $ object ["data" .= slideWitheEmbeds]
